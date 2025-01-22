e45eaa904de3e3ba325c206a7c36e08cd6c351dd
Evan <evan@Evans-MacBook-Pro.local>
2025-01-22 20:51:14 +0000

    feat: implement folder rendering and displaying data to pass tests

--- src/components/Folder/Folder.tsx
+++ src/components/Folder/Folder.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, FC } from 'react';
 
-const Folder = () => {
-  return (
-    <div>
-    </div>
-  );
-};
+interface FolderProps {
+    folder: {
+      id: number;
+      name: string;
+      type: 'folder';
+      files: {
+        id: number;
+        name: string;
+        type: 'folder' | 'file';
+        added: string; 
+      }[];
+    };
+  }
+
+  const Folder: FC<FolderProps> = ({ folder }) => {
+  
+    return (
+      <li>
+        <div className="folder">
+          📁 <strong>{folder.name}</strong> ({folder.files.length} items)
+        </div>
+      </li>
+    );
+  };
 
 export default Folder;
